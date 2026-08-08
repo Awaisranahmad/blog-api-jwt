0dc4603839d5074abf971d492ceb15d3b4f40276
Rana Awais Ahmad <rawaisah124@gmail.com>
2026-08-08 22:06:07 +0500

    add authroute

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-require('dotenv').config();
-const express= require('express');
-const mongoose= require('mongoose');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("../routes/authRoutes");
 
-const app= express();
-const PORT = process.env.PORT|| 5000;
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI).then(()=>console.log('blog database is connected')).catch((err)=>console.log('database error',err.message));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("blog database is connected"))
+  .catch((err) => console.log("database error", err.message));
 
-
-app.get('/',(req,res)=>{
-    res.send('blog API is running..........')
+app.get("/", (req, res) => {
+  res.send("blog API is running..........");
 });
-app.listen(PORT,()=>{
-    console.log(`blog server runnging http://localhost:${PORT}`);
-})
+
+app.use("/api/auth", authRoutes);
+app.listen(PORT, () => {
+  console.log(`blog server runnging http://localhost:${PORT}`);
+});
