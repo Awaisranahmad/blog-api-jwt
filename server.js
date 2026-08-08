b1b3648702166e5b2eb944c5da4c906e22b27b21
Rana Awais Ahmad <rawaisah124@gmail.com>
2026-08-08 22:19:33 +0500

    correction

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const authRoutes = require("../routes/authRoutes");
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
