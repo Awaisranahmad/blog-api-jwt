e805d6ebcaaa2481f3fe9bc1b0be00eaef0428fe
Rana Awais Ahmad <rawaisah124@gmail.com>
2026-08-11 12:36:58 +0500

    updation

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,27 +1,25 @@
 const jwt = require('jsonwebtoken');
-const asyncHandler = require('../utils/asyncHandler');
+const asyncHandler = require('../utils/asycHandler');
 
-// this function verify the token and user add the data in the req .user
-exports.portect = asyncHandler (async(req,res,next)=>{
-    let token;
-    // token is authorize token available in the header or not
-    if (
-        req.headers.authorization && req.headers.authorization.startsWith('Bearer')
-    ){
-        token = req.headers.authorization.spilts(' ')[1];
-        //bearer means <token> sparated out from token
-        if(!token){
-            res.status(401);
-            throw new Error('Not authorized, no token provided');
+// Yeh function token check karega
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
 
-        }
-        try {
-            const decoded = jwt.verify(token,process.env.JWT_SECRET);
-            req.user= decoded;
-            next();
-        } catch (error) {
-            res.status(401);
-            throw new Error('Not authorized, token invalid or expired');
-        }
-    }
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error('Not authorized, no token provided');
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // userId, email, username store ho jayenge
+    next();
+  } catch (error) {
+    res.status(401);
+    throw new Error('Not authorized, token invalid or expired');
+  }
 });
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,21 +1,21 @@
-const express  = require('express');
+const express = require('express');
 const router = express.Router();
-const {protect}= require('../middleware/authMiddleware')
+const { protect } = require('../middleware/authMiddleware');
+const {
+  getAllPosts,
+  getPostById,
+  createPost,
+  updatePost,
+  deletePost
+} = require('../controllers/postController');
 
-const {getAllPosts,getPostById
-    , createdPost,updatePost,deletePost 
-}=require('../controllers/postController');
+// Public
+router.get('/', getAllPosts);
+router.get('/:id', getPostById);
 
-//public routes not require any auth for that posts
+// Protected (Auth Middleware + Controllers)
+router.post('/', protect, createPost);
+router.put('/:id', protect, updatePost);
+router.delete('/:id', protect, deletePost);
 
-router.get('/',getAllPosts);
-router.get('/:id',getPostById);
-
-//portected routes
-router.post('/',protect,createdPost);
-router.put('/:id',protect,updatePost);
-router.delete('/:id',protect,deletePost);
-
-
-
-module.exports= router;
+module.exports = router;
