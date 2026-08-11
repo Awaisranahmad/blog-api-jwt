c91ca76ed42c6fef14a56ea704165dc7e37a6839
Rana Awais Ahmad <rawaisah124@gmail.com>
2026-08-11 11:08:26 +0500

    ad post routes in the server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const postRoutes = require('./routes/postRoutes');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
@@ -20,6 +21,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use('/api/posts',postRoutes);
 app.listen(PORT, () => {
   console.log(`blog server runnging http://localhost:${PORT}`);
 });
