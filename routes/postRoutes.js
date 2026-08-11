16447655421685ff8ae531863a5ed01db1c1e3d6
Rana Awais Ahmad <rawaisah124@gmail.com>
2026-08-11 11:53:14 +0500

    add CUD routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,21 @@
 const express  = require('express');
 const router = express.Router();
+const {protect}= require('../middleware/authMiddleware')
 
-const {getAllPosts,getPostById}=require('../controllers/postController');
+const {getAllPosts,getPostById
+    , createdPost,updatePost,deletePost 
+}=require('../controllers/postController');
 
 //public routes not require any auth for that posts
 
 router.get('/',getAllPosts);
 router.get('/:id',getPostById);
+
+//portected routes
+router.post('/',protect,createdPost);
+router.put('/:id',protect,updatePost);
+router.delete('/:id',protect,deletePost);
+
+
+
 module.exports= router;
